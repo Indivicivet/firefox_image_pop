acda61183aa03e0bbc44eaff235c67274b7842f5
Indivicivet <indivicivet@gmail.com>
2021-07-06 00:44:03 +0100

    ignore case and find many more file extensions

--- image_pop_thistab.js
+++ image_pop_thistab.js
@@ -1,6 +1,6 @@
 script_results = [];
 for (const script of document.getElementsByTagName("script")) {
-	urls = script.text.match(/"https[^"]+\.jpg"/)
+	urls = script.text.match(/"https[^"]+\.(?:jpg|jpeg|png|webp|gif|mp4|webm|wmv)"/i)
 	if (urls) {
 		script_results.push(
 			...urls.map((s) => {return decodeURIComponent(JSON.parse(s))})
