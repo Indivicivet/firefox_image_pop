e1f60f08661ef75682ac9281860a2000af51304c
Indivicivet <indivicivet@gmail.com>
2021-07-05 23:06:17 +0100

    add non-functional context menu

---
+++ image_pop_bg.js
@@ -0,0 +1,18 @@
+browser.contextMenus.create(
+	{
+		id: "image_pop",
+		title: "Image Pop",
+		contexts: ["all"]
+	},
+	() => {}
+);
+
+browser.contextMenus.onClicked.addListener(
+	(info, tab) => {
+		switch (info.menuItemId) {
+			case "image_pop":
+				console.log("pop!")
+				break;
+		}
+	}
+);
