6c04cc123fbf73a6b56d066af801ee630cd4545d
Indivicivet <indivicivet@gmail.com>
2021-07-06 00:40:45 +0100

    find .jpg files referred to in scripts

--- image_pop_thistab.js
+++ image_pop_thistab.js
@@ -1,1 +1,11 @@
-[...document.images].map((im) => {return im.src});
+script_results = [];
+for (const script of document.getElementsByTagName("script")) {
+	urls = script.text.match(/"https[^"]+\.jpg"/)
+	if (urls) {
+		script_results.push(
+			...urls.map((s) => {return decodeURIComponent(JSON.parse(s))})
+		)
+	};
+}
+
+script_results.concat([...document.images].map((im) => {return im.src}));
